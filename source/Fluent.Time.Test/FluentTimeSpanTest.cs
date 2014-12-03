2dbf3f163bb371a57142b1748cdef4a5d35cd088
Loc Tan Vo <loc.tan.vo@gmail.com>
2014-12-03 23:01:53 +0100

    Initial checkin

---
+++ source/Fluent.Time.Test/FluentTimeSpanTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Fluent.Time.Test
+{
+    [TestFixture]
+    public class FluentTimeSpanTest
+    {
+        [TestCase(1, 7)]
+        [TestCase(2, 14)]
+        [TestCase(3, 21)]
+        [TestCase(1.5, 10.5)]
+        public void Weeks_ParseCorrectly(double weeks, double expectedDays)
+        {
+            Assert.That(weeks.Weeks(), Is.EqualTo(TimeSpan.FromDays(expectedDays)));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(42)]
+        [TestCase(4.2)]
+        public void Days_ParseCorrectly(double value)
+        {
+            Assert.That(value.Days(), Is.EqualTo(TimeSpan.FromDays(value)));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(42)]
+        [TestCase(4.2)]
+        public void Hours_Integer_ParseCorrectly(double value)
+        {
+            Assert.That(value.Hours(), Is.EqualTo(TimeSpan.FromHours(value)));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(42)]
+        [TestCase(4.2)]
+        public void Minutes_ParseCorrectly(double value)
+        {
+            Assert.That(value.Minutes(), Is.EqualTo(TimeSpan.FromMinutes(value)));
+        }
+
+        [Test]
+        public void And_AddsTheTime()
+        {
+            Assert.That(2.Hours().And(10.Minutes()), Is.EqualTo(TimeSpan.FromMinutes(130)));
+        }
+    }
+}
---
+++ source/Fluent.Time/FluentTimeSpan.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Fluent.Time
+{
+    public static class FluentTimeSpan
+    {
+        public static TimeSpan Weeks(this double weeks)
+        {
+            return TimeSpan.FromDays(weeks * 7);
+        }
+
+        public static TimeSpan Days(this double days)
+        {
+            return TimeSpan.FromDays(days);
+        }
+
+        public static TimeSpan Hours(this double hours)
+        {
+            return TimeSpan.FromHours(hours);
+        }
+
+        public static TimeSpan Minutes(this double minutes)
+        {
+            return TimeSpan.FromMinutes(minutes);
+        }
+
+        public static TimeSpan Weeks(this int weeks)
+        {
+            return weeks.Double().Weeks();
+        }
+
+        public static TimeSpan Days(this int days)
+        {
+            return days.Double().Days();
+        }
+
+        public static TimeSpan Hours(this int hours)
+        {
+            return hours.Double().Hours();
+        }
+
+        public static TimeSpan Minutes(this int minutes)
+        {
+            return minutes.Double().Minutes();
+        }
+
+        public static TimeSpan And(this TimeSpan timeSpan, TimeSpan addition)
+        {
+            return timeSpan.Add(addition);
+        }
+
+        private static double Double(this int value)
+        {
+            return value;
+        }
+    }
+}
